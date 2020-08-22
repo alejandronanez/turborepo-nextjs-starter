a99817614acb133e8d272fdcd8a8ca67cfa54de9
Alejandro Nanez <alejonanez@gmail.com>
2020-08-22 16:36:10 -0500

    Update baseUrl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import { Greeting } from 'components/Greeting';
+import React from 'react';
 
 export default function Home() {
-  return <Greeting />;
+  return (
+    <>
+      <Greeting />
+    </>
+  );
 }
