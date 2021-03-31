ff4d3614185531b430bf3f92747cac2ee77cec88
Alejandro Nanez <alejonanez@gmail.com>
2021-03-31 10:37:46 -0500

    Deleted _document and moved logic to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import 'tailwindcss/tailwind.css';
+import React from "react";
+import Head from "next/head";
+import {GoogleAnalytics} from "../components/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Title - MAKE ME DYNAMIC PLEASE</title>
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <GoogleAnalytics />
+    </Head>
+    <Component {...pageProps} /></>;
 }
 
 export default MyApp;
