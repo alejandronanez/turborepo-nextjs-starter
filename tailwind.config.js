d1d74fc7c21291394d9af8f157309556031b7d1c
Alejandro Nanez <alejonanez@gmail.com>
2021-03-31 10:38:33 -0500

    Update tailwind.config.js and postcss.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 };
