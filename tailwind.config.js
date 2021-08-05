777aaf8b9ca512e750901fe6de49b7f47fcf5aac
Alejandro Nanez <alejonanez@gmail.com>
2021-08-05 15:07:56 -0500

    Remove JIT from tailwind as it messes up autocomplete in Webstorm.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  mode: 'jit',
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
