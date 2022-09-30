5d16f3decbb23da7dd0151dc30f167198983c9d0
Alejandro Nanez <git@alejandro.dev>
2022-09-30 15:28:07 -0500

    remove bundle analyzer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,1 @@
-const withPlugins = require('next-compose-plugins');
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withPlugins([[withBundleAnalyzer]], {});
+module.exports = {};
